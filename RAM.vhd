780b21da4464ceda2d036303af5bc8b166f5a87f
Welbert Hime <welberthime@hotmail.com>
2018-11-15 00:05:42 -0200

    Adicionando mais 4 saídas a RAM

--- RAM.vhd
+++ RAM.vhd
@@ -1,38 +1,36 @@
 -- Quartus II VHDL Template
--- Single-port RAM with single read/write address and initial contents	
+-- Single-port RAM with single read/write address and initial contents
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity RAM is
-
-	generic 
-	(
+	generic (
 		DATA_WIDTH : natural := 8;
 		ADDR_WIDTH : natural := 12
 	);
-
-	port 
-	(
+	port (
 		clk		: in std_logic;
 		addr	: in natural range 0 to 2**ADDR_WIDTH-1;
-		q		: out std_logic_vector((DATA_WIDTH -1) downto 0)
+		q		: out std_logic_vector((DATA_WIDTH -1) downto 0);
+		q1		: out std_logic_vector((DATA_WIDTH -1) downto 0);
+		q2		: out std_logic_vector((DATA_WIDTH -1) downto 0);
+		q3		: out std_logic_vector((DATA_WIDTH -1) downto 0);
+		q4		: out std_logic_vector((DATA_WIDTH -1) downto 0)
 	);
-
 end RAM;
 
 architecture rtl of RAM is
-
 	-- Build a 2-D array type for the RAM
 	subtype word_t is std_logic_vector((DATA_WIDTH-1) downto 0);
 	type memory_t is array(2**ADDR_WIDTH-1 downto 0) of word_t;
 
 	function init_ram
-		return memory_t is 
+		return memory_t is
 		variable tmp : memory_t := (others => (others => '0'));
-	begin 
-		for addr_pos in 0 to 2**ADDR_WIDTH - 1 loop 
+	begin
+		for addr_pos in 0 to 2**ADDR_WIDTH - 1 loop
 			-- Initialize each address with the address itself
 			tmp(addr_pos) := std_logic_vector(to_unsigned(0, DATA_WIDTH));
 		end loop;
@@ -47,24 +45,24 @@ architecture rtl of RAM is
 		--tmp(8) := "01100000"; -- iadd
 		--tmp(9) := "01010011"; -- istore_<3>
 		--tmp(10) := "10000000"; -- NOP
-		
+
 		tmp(0) := "00010000"; -- bipush
 		tmp(1) := "00000010"; -- 2
 		tmp(2) := "00010000"; -- bipush
 		tmp(3) := "00000011"; -- 3
 		tmp(4) := "10101111"; -- if_icmpEQ
-		tmp(5) := "00000000"; -- 
-		tmp(6) := "00000000"; -- 
+		tmp(5) := "00000000"; --
+		tmp(6) := "00000000"; --
 		tmp(7) := "10000000"; -- NOP
 		return tmp;
-	end init_ram;	 
+	end init_ram;
 
 	-- Declare the RAM signal and specify a default value.	Quartus II
-	-- will create a memory initialization file (.mif) based on the 
+	-- will create a memory initialization file (.mif) based on the
 	-- default value.
 	signal ram : memory_t := init_ram;
 
-	-- Register to hold the address 
+	-- Register to hold the address
 	signal addr_reg : integer range 0 to 2**ADDR_WIDTH-1;
 
 begin
@@ -78,5 +76,9 @@ begin
 	end process;
 
 	q <= ram(addr_reg);
+	q1 <= ram(addr_reg+1);
+	q2 <= ram(addr_reg+2);
+	q3 <= ram(addr_reg+3);
+	q4 <= ram(addr_reg+4);
 
 end rtl;
